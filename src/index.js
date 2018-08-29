b73985a8e1e22a4f7565c33cd36d4d776fba8836
endorphin82 <endorphin82@gmail.com>
2018-08-29 11:38:39 +0300

    React Router Setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
-import './assets/app.css'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
         <App />,
-document.getElementById('root')); 
+document.getElementById('root'));
+registerServiceWorker();
