ce951cad96a15943d229d868f28e4b0a97b21e11
endorphin82 <endorphin82@gmail.com>
2018-09-05 16:14:20 +0300

    Create Profile Form

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { setCurrentUser } from './actions/authActions';
 import Dashboard from "./components/dashboard/Dashboard";
 import {clearCurrentProfile} from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import './assets/app.css'
 
@@ -54,6 +55,9 @@ class App extends Component {
             <Route exact path="/login" component={Login}/>
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
             </Switch>
           </div>
           <Footer />
