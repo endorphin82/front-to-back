459089468831434c144579ce4323fe6572ecc6b3
endorphin82 <endorphin82@gmail.com>
2018-09-05 14:36:09 +0300

    Private Route Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { Provider } from 'react-redux';
 
@@ -13,8 +13,10 @@ import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser } from './actions/authActions';
 import Dashboard from "./components/dashboard/Dashboard";
 import {clearCurrentProfile} from "./actions/profileActions";
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import './assets/app.css'
+
 
 
 
@@ -50,7 +52,9 @@ class App extends Component {
           <div className="container">
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
-            <Route exact path="/dashboard" component={Dashboard}/>
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            </Switch>
           </div>
           <Footer />
         </div>
