7123e50e0b56bdd68168283f6b9686b47492d58b
endorphin82 <endorphin82@gmail.com>
2018-09-24 18:28:55 +0300

    PostsComments

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // mongoURI:'mongodb://brad:123456q@ds229312.mlab.com:29312/devconnector',
-   mongoURI: 'mongodb://localhost:27017/devconnector',
+  mongoURI:'mongodb://brad:123456q@ds229312.mlab.com:29312/devconnector',
+   // mongoURI: 'mongodb://localhost:27017/devconnector',
   secretOrKey : 'secret'
 };
